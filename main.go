0873f4e34c08a05bc5fbdf3daed6cf6c52b082e4
user <yudisbaek@gmail.com>
2023-05-27 05:00:33 +0700

    only scrape for http

--- main.go
+++ main.go
@@ -3,14 +3,63 @@ package main
 import (
 	"fmt"
 	"github.com/gocolly/colly"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"))
-	c.OnHTML(".mw-parser-output", func(e *colly.HTMLElement) {
-		links := e.ChildAttrs("a[href]", "href")
-		fmt.Println(links)
+	c := colly.NewCollector()
+
+	imageUrls := make([]string, 0)
+	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
+		imageUrl := e.Attr("src")
+		imageUrls = append(imageUrls, imageUrl)
 	})
-	c.Visit("https://en.wikipedia.org/wiki/Elon_Musk")
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("request url:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+	weburl := ""
+	err := c.Visit(weburl)
+	if err != nil {
+		log.Fatalf("Cannot visit %s", weburl)
+	}
+
+	for _, imageUrl := range imageUrls {
+		err := downloadImage(imageUrl)
+		if err != nil {
+			log.Println("Error downloading image:", err)
+		}
+	}
+	fmt.Println("Done!")
+}
+
+func downloadImage(imageUrl string) error {
+	if !strings.HasPrefix(imageUrl, "http://") && !strings.HasPrefix(imageUrl, "https://") {
+		fmt.Println("Skipping", imageUrl)
+	}
+	tokens := strings.Split(imageUrl, "/")
+	filename := tokens[len(tokens)-1]
+
+	resp, err := http.Get(imageUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Downloaded", imageUrl)
+	return nil
 }
