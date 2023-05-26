7d952c3dfebbb7f10d5cce18c4c14729b4eaffeb
user <yudisbaek@gmail.com>
2023-05-27 05:35:35 +0700

    add download directory

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	filepath2 "path/filepath"
 	"strings"
 )
 
@@ -21,6 +22,8 @@ func main() {
 	c.OnError(func(r *colly.Response, err error) {
 		log.Println("request url:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
+
+	//weburl is website url paste on that string
 	weburl := ""
 	err := c.Visit(weburl)
 	if err != nil {
@@ -40,8 +43,16 @@ func downloadImage(imageUrl string) error {
 	if !strings.HasPrefix(imageUrl, "http://") && !strings.HasPrefix(imageUrl, "https://") {
 		fmt.Println("Skipping", imageUrl)
 	}
+	//download directory the files will be on there
+	dirdownload := "./download"
+	err := os.MkdirAll(dirdownload, os.ModePerm)
+	if err != nil {
+		fmt.Println("Failed Create Directory", err)
+	}
 	tokens := strings.Split(imageUrl, "/")
 	filename := tokens[len(tokens)-1]
+
+	filepath := filepath2.Join(dirdownload, filename)
 
 	resp, err := http.Get(imageUrl)
 	if err != nil {
@@ -49,7 +60,7 @@ func downloadImage(imageUrl string) error {
 	}
 	defer resp.Body.Close()
 
-	file, err := os.Create(filename)
+	file, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
